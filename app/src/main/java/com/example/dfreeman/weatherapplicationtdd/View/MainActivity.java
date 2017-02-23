7e3bb84dac8a30190a884c5295fe50824a5fb54d
dfreemanRIIS <dfreeman@riis.com>
2017-02-23 15:29:32 -0500

    3 - Enter and Pull Location Feature TEST SUCCESS

--- app/src/main/java/com/example/dfreeman/weatherapplicationtdd/View/MainActivity.java
+++ app/src/main/java/com/example/dfreeman/weatherapplicationtdd/View/MainActivity.java
@@ -1,15 +1,61 @@
 package com.example.dfreeman.weatherapplicationtdd.View;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.dfreeman.weatherapplicationtdd.Controller.Controller;
 import com.example.dfreeman.weatherapplicationtdd.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView displayLocation;
+    private Controller controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controller = new Controller();
+
+        //Set Location
+        Controller controller = new Controller();
+        displayLocation = (TextView)findViewById(R.id.displayLocation);
+        displayLocation.setText(controller.getLocation());
+    }
+
+    public void onSetLocationButton(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Location");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String[] m_Text = new String[1];
+                m_Text[0] = input.getText().toString();
+                controller.setLocation(m_Text[0]);
+                displayLocation.setText(controller.getLocation());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
