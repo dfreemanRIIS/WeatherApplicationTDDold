23276c55b4b8eb047efb78de1c96e8cdfd85b301
dfreemanRIIS <dfreeman@riis.com>
2017-02-23 13:22:29 -0500

    MVC Template

---
+++ app/src/main/java/com/example/dfreeman/weatherapplicationtdd/Controller/Controller.java
@@ -0,0 +1,8 @@
+package com.example.dfreeman.weatherapplicationtdd.Controller;
+
+/**
+ * Created by dfreeman on 2/23/17.
+ */
+
+public class Controller {
+}
---
+++ app/src/main/java/com/example/dfreeman/weatherapplicationtdd/Model/DatabaseHelper.java
@@ -0,0 +1,8 @@
+package com.example.dfreeman.weatherapplicationtdd.Model;
+
+/**
+ * Created by dfreeman on 2/23/17.
+ */
+
+public class DatabaseHelper {
+}
