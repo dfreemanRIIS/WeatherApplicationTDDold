284f3597fcf4bf413df054196cf4b41c1193db61
dfreemanRIIS <dfreeman@riis.com>
2017-02-23 13:50:50 -0500

    2 - Controller Feature TEST SUCCESS

--- app/src/main/java/com/example/dfreeman/weatherapplicationtdd/Controller/Controller.java
+++ app/src/main/java/com/example/dfreeman/weatherapplicationtdd/Controller/Controller.java
@@ -1,8 +1,19 @@
 package com.example.dfreeman.weatherapplicationtdd.Controller;
 
-/**
- * Created by dfreeman on 2/23/17.
- */
+import com.example.dfreeman.weatherapplicationtdd.Model.LocationModel;
 
 public class Controller {
+    LocationModel locationModel;
+
+    public Controller() {
+        locationModel = new LocationModel();
+    }
+
+    public String getLocation() {
+        return locationModel.getLocation();
+    }
+
+    public void setLocation(String location) {
+        locationModel.setLocation(location);
+    }
 }
